aa4fda137753dfe968357f66508cba0b63192961
Vandana Yadav <67888275+vandu565@users.noreply.github.com>
2020-10-10 14:47:05 +0530

    Create reverseStackByRecursion.cpp

---
+++ reverseStackByRecursion.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void atBottom(stack<int>&s, int x){
+	if(s.empty()){
+		s.push(x);
+		return;
+	}
+	int y=s.top();
+	s.pop();
+	atBottom(s,x);
+	s.push(y);
+}
+
+void reverseStackByRecursion(stack<int>& s){
+	if(s.empty())
+	return;
+	int x=s.top();
+	s.pop();
+	reverseStackByRecursion(s);
+	atBottom(s,x);
+}
+
+int main()
+{
+	stack<int> s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+	reverseStackByRecursion(s);
+	
+	while(s.empty()==false){
+		cout<< s.top() <<" ";
+		s.pop();
+	}
+	return 0;
+}
